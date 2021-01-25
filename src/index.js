256ba383afb64fd10b4c874809bfb03240706776
amareshsm <30730124+amareshsm@users.noreply.github.com>
2021-01-25 13:50:28 +0530

    Revert "Revert ":hammer: bug fix""
    This reverts commit 057b9075c3a5b32d8ed9c595bb58ee0c2acf2913.

--- src/index.js
+++ src/index.js
@@ -1,50 +1,52 @@
 const axios = require('axios')
-let url = require('url')
-let crypto = require('crypto')
-let events = require('events')
-let qs = require('qs')
+const crypto = require('crypto')
+const events = require('events')
+const qs = require('qs')
 
 module.exports = function (params) {
-  let state = crypto.randomBytes(8).toString('hex')
-  if (!params.redirectURI)
-    params.redirectURI = 'http://localhost:3000/callback/'
+  const state = crypto.randomBytes(8).toString('hex')
+  if (!params.baseURL) params.baseURL = 'http://localhost:3000'
+  if (!params.redirectPath) params.redirectPath = '/callback/'
+  const redirectURL = new URL(params.redirectPath, params.baseURL)
   if (typeof params.scope === 'undefined')
     params.scope = 'surveys_read'
-  let emitter = new events.EventEmitter()
+  const emitter = new events.EventEmitter()
 
-  function login(req, res) {
-    let u =
+  function authorize(req, res) {
+    console.log('rURL', redirectURL)
+    const url =
       'https://api.surveymonkey.com/oauth/authorize' +
       '?client_id=' +
       params.clientId +
       '&redirect_uri=' +
-      params.redirectURI +
+      redirectURL +
       '&response_type=code' +
       (params.scope ? '&scope=' + params.scope : '') +
       '&state=' +
       state
     res.statusCode = 302
-    res.setHeader('location', u)
+    res.setHeader('location', url)
     res.end()
   }
 
-  function callback(req, resp) {
-    var query = url.parse(req.url, true).query
-    var code = query.code
+  function generateToken(req, resp) {
+    const codeURL = new URL(req.url, params.baseURL)
+    const code = codeURL.searchParams.get('code')
+    console.log('code', codeURL.searchParams.get('code'))
     if (!code)
       return emitter.emit(
         'error',
         { error: 'missing oauth code' },
         resp,
       )
-    let data = qs.stringify({
+    const data = qs.stringify({
       client_secret: `${params.clientSecret}`,
       code: `${code}`,
-      redirect_uri: `${params.redirectURI}`,
+      redirect_uri: `${redirectURL}`,
       client_id: `${params.clientId}`,
       grant_type: `authorization_code`,
     })
-    let config = {
+    const config = {
       method: 'post',
       url: 'https://api.surveymonkey.com/oauth/token',
       headers: {
@@ -52,25 +54,16 @@ module.exports = function (params) {
       },
       data: data,
     }
-    console.log('config', config)
     axios(config)
       .then(function (response) {
-        console.log(response.data)
         return emitter.emit('success', response, resp)
       })
       .catch(function (error) {
-        console.log(error)
         return emitter.emit('error', error, resp)
       })
   }
 
-  function testfn(param) {
-    console.log(param)
-    return emitter.emit('Testing', { data: param })
-  }
-
-  emitter.login = login
-  emitter.testfn = testfn
-  emitter.callback = callback
+  emitter.authorize = authorize
+  emitter.generateToken = generateToken
   return emitter
 }
