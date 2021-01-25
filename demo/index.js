ad594386252bbe2e61998b7040114e6da065eab9
amareshsm <30730124+amareshsm@users.noreply.github.com>
2021-01-25 13:48:33 +0530

    :hammer: bug fix

--- demo/index.js
+++ demo/index.js
@@ -2,40 +2,29 @@ const http = require('http')
 const port = 3000
 const hostname = '127.0.0.1'
 const lib = require('survey-monkey-oauth')({
-  clientId: 'your client id',
-  clientSecret: 'your secret key',
-  redirectURI: 'http://localhost:3000/callback/',
+  clientId: 'RwNDD9rjQTuIvioTN-SkeQ',
+  clientSecret: '117615308690000773230667792595522339738',
+  baseURL: 'http://localhost:3000/',
+  redirectPath: '/callback/',
 })
+// on success
+lib.on('success', (tokenRes, res) => {
+  console.log('success !!!', tokenRes.data)
+  res.end('token successfully generated')
+})
+
+// on failure
+lib.on('error', (errorRes, res) => {
+  console.log('error', errorRes)
+})
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-
-  //on success
-  lib.on('success', (tokenRes, tokenReq) => {
-    console.log('success', tokenRes.data)
-    res.end('token successfully generated')
-  })
-
-  //on failure
-  lib.on('error', (res, req) => {
-    console.log('error', res, req)
-  })
-
-  if (req.url.match(/login/)) {
-    return lib.login(req, res)
-  }
-  if (req.url.match(/callback/)) {
-    return lib.callback(req, res)
-  }
-
-  if (req.url.match(/test/)) {
-    lib.on('Testing', (data) => {
-      console.log('test route', data)
-    })
-    lib.testfn('sample data')
-    res.end('testing')
-  }
+  if (req.url.match(/login/)) return lib.authorize(req, res)
+  if (req.url.match(/callback/)) return lib.generateToken(req, res)
 })
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
 })
